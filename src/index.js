a5fcb4ba478784330ff4ef43d28c4e9e08acdb85
kenny <kenny.kwyiu@gmail.com>
2024-02-14 23:17:11 +0800

    revised

--- src/index.js
+++ src/index.js
@@ -18,12 +18,12 @@ function Test() {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
     <StarRating size={24} color="red" className="test" defaultRating={3} />
-    <Test /> */}
+    <Test />  */}
   </React.StrictMode>
 );
