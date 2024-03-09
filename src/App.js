f392e0798062ea7ebbbb9e2a0085b0bfc6141269
kenny <kenny.kwyiu@gmail.com>
2024-03-09 17:14:36 +0800

    167-fixed the bug by validation

--- src/App.js
+++ src/App.js
@@ -175,6 +175,7 @@ function Search({ query, setQuery }) {
 
   useEffect(function () {
     function callback(e) {
+      if (document.activeElement === inputEl.current) return;
       if (e.code === "Enter") {
         inputEl.current.focus();
         setQuery("");
