8b39eaef7a760db8ec9e81bece8ecfadb20bb813
kenny <kenny.kwyiu@gmail.com>
2024-03-11 21:13:35 +0800

    170-implemented custom hook

--- src/useMovies.js
+++ src/useMovies.js
@@ -1,1 +1,56 @@
-export function useMovies() {}
+import { useState, useEffect } from "react";
+
+const KEY = "7e0e4cf3";
+
+export function useMovies(query) {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  useEffect(
+    function () {
+      const controller = new AbortController();
+
+      async function fetchMovies() {
+        try {
+          setIsLoading(true);
+          setError("");
+          const res = await fetch(
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
+          );
+
+          if (!res.ok)
+            throw new Error("Something went wrong with fetching moving");
+
+          const data = await res.json();
+          if (data.Response === "False") throw new Error("Movie not found");
+          console.log(data.Search);
+          setMovies(data.Search);
+          setError("");
+        } catch (error) {
+          if (error.name !== "AbotyError") {
+            console.log(error.message);
+            setError(error.message);
+          }
+        } finally {
+          setIsLoading(false);
+        }
+      }
+
+      if (query.length < 3) {
+        setMovies([]);
+        setError("");
+        return;
+      }
+      // handleCloseMovie();
+      fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
+    },
+    [query]
+  );
+
+  return { movies, isLoading, error };
+}
